ba8b6ce15bc5697632a1ff319ff4bb97a88fdf24
jessicawa <42417374+jessicawa@users.noreply.github.com>
2021-07-13 18:45:17 -0600

    added bernoulli, binomial, normal, triangular, exponential, poisson, and weibull distributions

--- random_distributions.py
+++ random_distributions.py
@@ -16,3 +16,83 @@ def unif(a, b):
 
     U = random()
     return (U+ a/(b-a))*(b-a)
+
+def bern(p):
+    """
+    Generate a bernoulli random variate from a standard uniform distribution
+    """
+    
+    U = unif(0, 1)
+    return int(U <= p)
+
+def binom(n, p):
+    """
+    Generate a binomial random variate that returns the number of successes in n trials, 
+    with p probability of success, from a standard uniform distribution.
+    """
+    trials = []
+    for _ in range(n):
+        U = unif(0, 1)
+        trials.append(int(U <= p))
+    return np.sum(trials)
+    
+def norm(mu, sigma, trig="cos"):
+    """
+    Generate a normal random variates with mean, mu, and standard deviation, sigma, 
+    using the Box-Muller transformation from a standard uniform distribution
+    """
+    U1 = unif(0, 1)
+    U2 = unif(0, 1)
+    
+    if trig == "sin":
+        z = np.sqrt(-2*np.log(U1))*np.sin(2*np.pi*U2)
+    else:
+        z = np.sqrt(-2*np.log(U1))*np.cos(2*np.pi*U2)
+    
+    return mu + sigma*z
+
+def tria(a, c):
+    """
+    Generate a triangualar random variate with min=a, max=c, and most likely, b, being halfway
+    between a and c. The random variate is found using a standard uniform distribution.
+    """
+    b = (a+c) / 2
+    U = lcg()
+    
+    if U < 0.5:
+        return np.sqrt(U*(c - a)*(b - a)) + a
+    else:
+        return c - np.sqrt((1 - U)*(c - a)*(c - b))
+    
+def expo(lamb):
+    """
+    Generate an exponential random variate with l = lambda, using U from a standard uniform
+    distribution.
+    """
+    U = unif(0,1)
+    
+    return -(1/lamb)*np.log(U)
+    
+def poisson(lamb):
+    """
+    Generate a poisson random variate with l = lambda, using U from a standard uniform distribution
+    and the exponential distribution as the interarrival times. n is the  number before thesum of 
+    exponential random variates is greater than 1.
+    """
+    sum_expo = 0
+    n = -1
+    
+    while (sum_expo < 1):
+        e = expo(lamb)
+        sum_expo += e
+        n += 1
+    return n
+    
+def weibull(lamb, beta):
+    """
+    Generate a weibull random variate with lambda = lamb and beta, using a standard uniform
+    distribution.
+    """
+    U = unif(0,1)
+    
+    return (1/lamb)*(-np.log(U))^(1/beta)
